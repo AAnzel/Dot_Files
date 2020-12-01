6595fc78f2a3f1b9aa88d5cfe5807925b298fd12
AAnzel <44969003+AAnzel@users.noreply.github.com>
2020-12-01 22:00:34 +0100

    Changing to absolute paths

--- .conky_configs/conky_start.sh
+++ .conky_configs/conky_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-conky -q -d -c /home/alexein/.conky_configs/.conky_old &
-conky -q -d -c /home/alexein/.conky_configs/.conky_time &
-conky -q -d -c /home/alexein/.conky_configs/.conky_todo &
+conky -q -d -c ~/.conky_configs/.conky_old &
+conky -q -d -c ~/.conky_configs/.conky_time &
+conky -q -d -c ~/.conky_configs/.conky_todo &
