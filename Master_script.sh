b7379542c4d18a7bbf549cf9e4695b081dfdcc96
AAnzel <44969003+AAnzel@users.noreply.github.com>
2022-04-24 18:36:20 +0200

    Added: Added more apt packages for ricing.

--- Master_script.sh
+++ Master_script.sh
@@ -13,7 +13,7 @@ sudo apt full-upgrade
 # 1.0: installing all of the programs
 echo
 echo "================= Installing programs ================="
-sudo apt install -y vlc qbittorrent gnome-tweaks steam neofetch conky rclone keepassxc lutris openconnect network-manager-openconnect network-manager-openconnect-gnome ruby-full imagemagick mpv cava htop
+sudo apt install -y vlc qbittorrent gnome-tweaks steam neofetch conky rclone keepassxc lutris openconnect network-manager-openconnect network-manager-openconnect-gnome ruby-full imagemagick mpv cava htop optipng sassc inkscape
 sudo gem install bundler
 flatpak install --noninteractive -y flathub com.viber.Viber com.skype.Client org.signal.Signal com.vscodium.codium com.microsoft.Teams
 
